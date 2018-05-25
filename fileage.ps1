194473bcc8f79e956ed1d11f33902d1fb93d240c
Arie <38727432+T100D@users.noreply.github.com>
2018-05-25 10:48:30 +0200

    Update fileage.ps1

--- fileage.ps1
+++ fileage.ps1
@@ -1,7 +1,5 @@
-'''
 $file = ‘c:\logfile.log'
    if (Test-Path $file -OlderThan (Get-Date).Addminutes(-150))
     {  "1 LOGFILE_LOG = logfile.log not updated for at least 2,5 hours"}
     Else
     {  "0 LOGFILE_LOG = logfile.log updated”}
-'''
