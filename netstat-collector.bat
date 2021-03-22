e7f03ec84ca7ede5e73d75b28d135896e4547c48
Arie <38727432+T100D@users.noreply.github.com>
2021-03-22 10:40:00 +0100

    Added windows only commands

--- netstat-collector.bat
+++ netstat-collector.bat
@@ -13,3 +13,13 @@ set command="netstat -an | grep <ip-addres> | wc -l"
 for /f "delims=" %%c in ('%command%') do (
 echo P TCP_Connecties_Server Count=%%c;50
 )
+
+
+Or in windows style:
+
+@echo off
+for %%p in (2580 2590 2591) do (
+for /f "delims=" %%c in ('netstat -an ^| findstr /v /i LISTENING ^| find /i /c ":%%p "') do (
+echo P TCP_Connections_%%p Connections_%%p=%%c Connections: %%c
+)
+)
