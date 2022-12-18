bf41d2e2ba1ac474d866827279c8dc46ceb0c411
Arie <38727432+T100D@users.noreply.github.com>
2022-12-18 20:04:19 +0100

    Create login-v2.bat

---
+++ login-v2.bat
@@ -0,0 +1,12 @@
+@echo off
+REM Local check to check if theire are active users on a system
+REM Improoved version after checking with ChatGPT
+REM
+REM Count the number of active logins
+for /f "delims=" %%c in ('query session ^| find /C "Active"') do (
+  set count=%%c
+)
+REM Prepairthe active quiry to check_mk
+for /f "delims=" %%c in ('%command%') do (
+echo P Logins count=%%c;0;1 %%c logins on system
+)
