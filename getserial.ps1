81ff2fadc24dabfad6fd25372385cdf2efb198d8
Arie <38727432+T100D@users.noreply.github.com>
2023-08-01 11:57:12 +0200

    Delete getserial.ps1

--- getserial.ps1
+++
@@ -1,9 +0,0 @@
-# script to report the hardware serial number
-# written by Arie van den Heuvel
-# 31-07-2023
-# get the serial number
-$serialnumber = (Get-WmiObject win32_bios | select -ExpandProperty Serialnumber)
-#
-# report the serial number to check_mk
-echo "<<<local>>>"
-echo "0 HW_Serialnumber - Hardware Serial Number=$serialnumber"
