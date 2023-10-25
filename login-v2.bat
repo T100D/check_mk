51e962d1f1308c4076a428ae312b4f564dee42fe
Arie <38727432+T100D@users.noreply.github.com>
2023-10-25 17:01:16 +0200

    Update login-v2.bat

--- login-v2.bat
+++ login-v2.bat
@@ -6,7 +6,7 @@ REM Count the number of active logins
 for /f "delims=" %%c in ('query session ^| find /C "Active"') do (
   set count=%%c
 )
-REM Prepairthe active quiry to check_mk
+REM Prepair the active query to check_mk
 for /f "delims=" %%c in ('%command%') do (
 echo P Logins count=%%c;0;1 %%c logins on system
 )
