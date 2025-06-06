c9297b2a6a80e822fa37a28e69528b3884e2f86c
Arie <38727432+T100D@users.noreply.github.com>
2025-06-06 15:22:13 +0200

    Create radarcheck-40.-rewritten.sh

---
+++ radarcheck-40.-rewritten.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Created by Arie
+#
+# To monitori radar status remotely
+#
+# 01-01-2016 created for monitoring radar sensor info
+#
+# 12-09-2021 V1.1 adjested for MC3.5 OPC command
+# 16-09-2021 Adjusted for stopped applciation (timout added)
+# 25-02-2024 aangepast voor MC 4.x software
+# 06-06-2025 Rewritten with ihelp of open-hands to be faster
+#
+# In case of a new non serial connected sensor remove antennestatus & transmitter status
+#
+
+
+timeout 5s /opt/vtmis/HTP/bin/x86_64-linux-gnu/ve4/opc localhost:htp_opc1 -c vp_status > /tmp/vp_status
+antennestatus=$(awk '/antenna/ && /on/' /tmp/vp_status | wc -l)
+        if [ $antennestatus -eq 1 ] ; then
+        echo "0 Radar_Antenne_Status status=$antennestatus OK"
+        else
+        echo "2 Radar_Antenne_Status status=$antennestatus NOK"
+        fi
+
+transmitterstatus=$(awk '/transmitter/ && /on/' /tmp/vp_status | wc -l)
+        if [ $transmitterstatus -eq 1 ] ; then
+        echo "0 Radar_Transmitter_Status status=$transmitterstatus OK"
+        else
+        echo "2 Radar_Transmitter_Status status=$transmitterstatus NOK"
+        fi
+
+videoactivity=$(awk '/video_activity/ && /nok/' /tmp/vp_status | wc -l)
+        if [ $videoactivity -eq 1 ] ; then
+        echo "2 Radar_Video_Activity status=0 NOK"
+        else
+        echo "0 Radar_Video_Activity status=1 OK"
+        fi
+
+northpulse=$(awk '/np_present/ && /yes/' /tmp/vp_status | wc -l)
+        if [ $northpulse -eq 1 ] ; then
+        echo "0 Radar_North_Pulse status=$northpulse OK"
+        else
+        echo "2 Radar_North_Pulse status=$northpulse NOK"
+        fi
+
+acp=$(awk '/acp_present/ && /yes/' /tmp/vp_status | wc -l)
+        if [ $acp -eq 1 ] ; then
+        echo "0 Radar_ACP_Present status=$acp OK"
+        else
+        echo "2 Radar_ACP_Present status=$acp NOK"
+        fi
