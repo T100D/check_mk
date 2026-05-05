e3c801928756a66a3e5a9d8c73ca0c50b8fb9c4c
Arie <38727432+T100D@users.noreply.github.com>
2026-05-05 15:36:07 +0200

    Refactor radar status checks for better parsing
    Updated status checks to use awk for parsing and improved output handling and reading  vp_status

--- radarcheck-40.sh
+++ radarcheck-40.sh
@@ -5,48 +5,50 @@
 # dd 16-09-2021 timeout toegevoed voor het geval een node getopt wordt en het script faalt
 # dd 25-02-2024 aangepast voor MC 4.x software
 # dd 16-04-2026 Omgeschreven van file based naar memory based met variable vpstatus
+# dd 05-05-2026 Verwerking uitlezen statusvariabele verbeterd met Claude Sonnet 4.6
 #
 
-vpstatus=$(timeout 5s /opt/vtmis/HTP/bin/x86_64-linux-gnu/ve4/opc localhost:htp_opc1 -c vp_status)
+vpstatus=$(timeout 5s /opt/vtmis/HTP/bin/x86_64-linux-gnu/ve4/opc localhost:htp_opc1 -c vp_status | tr -d '\r' | tr ' ' '\n')
 
-antennestatus=$(echo $vpstatus | grep antenna | grep on | wc --lines)
-    if [ $antennestatus -eq 1 ] ; then
+antennestatus=$(echo "$vpstatus" | awk -F': ' '/antenna/{print $NF}' | tr -d ' \r')
+if [ "$antennestatus" = "on" ]; then
     echo "0 Radar_Antenne_Status status=$antennestatus OK"
     else
     echo "2 Radar_Antenne_Status status=$antennestatus NOK"
-    fi
+fi
 
-transmitterstatus=$(echo $vpstatus | grep transmitter | grep on | wc --lines)
-        if [ $transmitterstatus -eq 1 ] ; then
-        echo "0 Radar_Transmitter_Status status=$transmitterstatus OK"
-        else
-        echo "2 Radar_Transmitter_Status status=$transmitterstatus NOK"
-        fi
+transmitterstatus=$(echo "$vpstatus" | awk -F': ' '/transmitter/{print $NF}' | tr -d ' \r')
+if [ "$transmitterstatus" = "on" ]; then
+   echo "0 Radar_Transmitter_Status status=$transmitterstatus OK"
+   else
+   echo "2 Radar_Transmitter_Status status=$transmitterstatus NOK"
+fi
 
-videoactivity=$(echo $vpstatus | grep video_activity | grep 'nok' | wc --lines)
-        if [ $videoactivity -eq 1 ] ; then
-        echo "2 Radar_Video_Activity status=0 NOK"
-        else
-        echo "0 Radar_Video_Activity status=1 OK"
-        fi
+videoactivity=$(echo "$vpstatus" | awk -F': ' '/video_activity/{print $NF}' | tr -d ' \r')
+if [ "$videoactivity" = "on" ]; then
+    echo "0 Radar_Video_Activity status=$videoactivity OK"
+    else
+    echo "2 Radar_Video_Activity status=$videoactivity NOK"
+fi
 
-northpulse=$(echo $vpstatus | grep np_present | grep yes | wc --lines)
-        if [ $northpulse -eq 1 ] ; then
-        echo "0 Radar_North_Pulse status=$northpulse OK"
-        else
-        echo "2 Radar_North_Pulse status=$northpulse NOK"
-        fi
+northpulse=$(echo "$vpstatus" | awk -F': ' '/np_present/{print $NF}' | tr -d ' \r')
+if [ "$northpulse" = "yes" ]; then
+    echo "0 Radar_North_Pulse status=$northpulse OK"
+    else
+    echo "2 Radar_North_Pulse status=$northpulse NOK"
+fi
 
-acp=$(echo $vpstatus | grep acp_present | grep yes | wc --lines)
-        if [ $acp -eq 1 ] ; then
-        echo "0 Radar_ACP_Present status=$acp OK"
-        else
-        echo "2 Radar_ACP_Present status=$acp NOK"
-        fi
+acp=$(echo "$vpstatus" | awk -F': ' '/acp_present/{print $NF}' | tr -d ' \r')
+if [ "$acp" = "yes" ]; then
+    echo "0 Radar_ACP_Present status=$acp OK"
+    else
+    echo "2 Radar_ACP_Present status=$acp NOK"
+fi
 
-sync=$(echo $vpstatus | grep sync_present | grep yes | wc --lines)
-        if [ $acp -eq 1 ] ; then
-        echo "0 Radar_SYNC_Present status=$acp OK"
-        else
-        echo "2 Radar_SYNC_Present status=$acp NOK"
-        fi
+sync=$(echo "$vpstatus" || awk -F': ' '/sync_present/{print $NF}' | tr -d ' \r')
+if [ "$sync" = "yes" ]; then
+    echo "0 Radar_SYNC_Present status=$acp OK"
+    else
+    echo "2 Radar_SYNC_Present status=$acp NOK"
+fi
+
