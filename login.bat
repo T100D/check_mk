67de127da4acecdf4d22eb3b2904b23666ebe5e1
Arie <38727432+T100D@users.noreply.github.com>
2026-07-18 19:20:20 +0200

    Add active user count check to login.bat
    Added a local check to count active users on the system.

--- login.bat
+++ login.bat
@@ -1,4 +1,5 @@
 @echo off
+REM Local check to check if there are active users on a system
 set command="query session | find /C "Active""
 for /f "delims=" %%c in ('%command%') do (
 echo P Logins count=%%c;0;1 %%c logins on system
