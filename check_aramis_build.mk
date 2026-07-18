97584de7b7a7b995c930f30dd3bfa02ab064a143
Arie <38727432+T100D@users.noreply.github.com>
2026-07-18 19:18:51 +0200

    Enhance comments in check_aramis_build.mk
    Added comments to clarify the purpose and usage of the script.

--- check_aramis_build.mk
+++ check_aramis_build.mk
@@ -1,13 +1,16 @@
 #
 # Created by Arie
+# A local check written to be used within check_mk
+# 
 # Script to read the export and state the build number of a programm
+# that belongs to the HITT/SAAB/Tidalis architecture.
+# eval aramis is available on port 51555 check /etc/service for others.
 #
 # Date 26-01-2026 Initial version
 # Date 16-07-2026 Rewritten to get the number to a memory based check
 #
 # Get the build number from eval-aramis and put the number in a file
 build=$(echo 'QUIT' | nc -4 -w 1  127.0.0.1 51555 | grep build)
-#
 #
 # Report the build number to Check_mk
 echo "0 ARAMIS_Build - ARAMIS Build =$build"
